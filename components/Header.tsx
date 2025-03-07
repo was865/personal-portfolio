828eb23517a9595685f6414be9832efc56f020a9
was865 <wangansen865@gmail.com>
2025-03-07 22:52:17 +0800

    style: fix for tailwind v3 to v4

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,10 +15,9 @@ function Header() {
   return (
     <header className="z-999 relative">
       <motion.div
-        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-40
-        shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
-        initial={{ y: -100, x: 0, opacity: 0 }}
-        animate={{ y: 0, x: 0, opacity: 1 }}
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white/40 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full dark:bg-gray-950/75 dark:border-black/40"
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
       ></motion.div>
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
@@ -32,7 +31,7 @@ function Header() {
               <Link
                 href={link.hash}
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 no-wrap hover:text-gray-950 dark:hover:text-gray-300 transition",
+                  "flex w-full items-center justify-center px-3 py-3 whitespace-nowrap hover:text-gray-950 dark:hover:text-gray-300 transition",
                   {
                     "text-gray-950": activeSection === link.name,
                     "dark:hover:text-gray-600": activeSection == link.name,
