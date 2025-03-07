3ef98cc63c485bc5c2023374b8353ce66de997b7
was865 <wangansen865@gmail.com>
2025-03-08 01:45:54 +0800

    fix: style classname don't work for tailwind v4

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,7 +15,7 @@ function Header() {
   return (
     <header className="z-999 relative">
       <motion.div
-        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white/40 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full dark:bg-gray-950/75 dark:border-black/40"
+        className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white/40 bg-white/40 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full dark:bg-gray-950/75 dark:border-black/40"
         initial={{ y: -100, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
       ></motion.div>
