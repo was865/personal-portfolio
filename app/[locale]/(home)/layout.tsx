a27fc83364bbbffcfe64ac38a666d81b0c8d443e
was865 <wangansen865@gmail.com>
2025-03-09 19:32:53 +0800

    chore: route groups

--- app/[locale]/(home)/layout.tsx
+++ app/[locale]/(home)/layout.tsx
@@ -1,6 +1,5 @@
 import { use } from "react";
 import Header from "@/components/Header"
-import { Inter } from "next/font/google"
 import ThemeContextProvider from "@/context/theme-context"
 import { ActionSectionContextProvider } from "@/context/action-section-context"
 import Footer from "@/components/Footer"
@@ -9,8 +8,7 @@ import ThemeSwitch from "@/components/ThemeTwich"
 import LanguageSwitch from "@/components/LanguageSwitch"
 import { NextIntlClientProvider, useMessages } from "next-intl"
 import WidgetWrapper from "@/components/WidgetWrapper"
-
-const inter = Inter({ subsets: ["latin"] })
+import { fontOleoScript, fontInter, fontUbuntu } from "@/config/fonts";
 
 export default function LocaleLayout(
   props: {
@@ -34,7 +32,7 @@ export default function LocaleLayout(
   return (
     <html lang={locale} className="scroll-smooth! relative">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        className={`${fontUbuntu.className} ${fontOleoScript.variable} bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div className="bg-[#ffe99b] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#5b3b3c]"></div>
         <div className="bg-[#b9f1fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#433f68]"></div>
